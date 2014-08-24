2a1742393219f5032c9f58b44469beaa1de5f402
jakenotjacob <jake.campbell91@gmail.com>
2014-08-24 10:06:31 -0600

    Add simple decoder in ruby and c.  Start largest word puzzle.

---
+++ LargestWord/largest.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+#hello yyyyyyy yzyzyzyzyzyz mellow well yo kellow lellow abcdef is yellow just 
+#here to add strings fellow lellow llleow
+#=>l e l o h m f y z a b w
+
+#sad das day mad den foot ball down touch pass play
+#=>z a d f o nhi
+
+
+
+words = gets.chomp.split(" ")
+letters = gets.chomp.split(" ")
+
+
+
---
+++ SimpleDecoder/decoder.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+// I have encoded a message by adding a "4" to each character's ASCII value.
+// It will be your job to decode this message by reversing the process and 
+// making a decoder.
+
+char *msg = "Etvmp$Jsspw%%%%\
+[e}$xs$ks%$]sy$lezi$wspzih$xli$lmhhir$qiwweki2$Rs{$mx$mw$}syv$xyvr$xs$nsmr\
+mr$sr$xlmw$tvero2$Hs$rsx$tswx$er}xlmrk$xlex${mpp$kmzi$e{e}$xlmw$qiwweki2$Pix\
+tistpi$higshi$xli$qiwweki$sr$xlimv$s{r$erh$vieh$xlmw$qiwweki2$]sy$ger$tpe}$epsrk\
+f}$RSX$tswxmrk$ls{$}sy$higshih$xlmw$qiwweki2$Mrwxieh$tswx$}syv$wspyxmsr$xs$fi$}syv\
+jezsvmxi$Lipps${svph$tvskveq$mr$sri$perkyeki$sj$}syv$glsmgi2$\
+Qeoi$wyvi$}syv$tvskveq$we}w$&Lipps$[svph%%%&${mxl$7$%$ex$xli$irh2$Xlmw${e}\
+tistpi$fvs{wmrk$xli$gleppirki${mpp$xlmro${i$lezi$epp$pswx$syv$qmrhw2$Xlswi${ls$tswx$lipps\
+{svph$wspyxmsrw${mxlsyx$xli$xlvii$%%%${mpp$lezi$rsx$higshih$xli$qiwweki$erh$ws$}sy$ger$\
+tspmxip}$tsmrx$syx$xlimv$wspyxmsr$mw$mr$ivvsv$,xli}$evi$nywx$jspps{mrk$epsrk${mxlsyx$ors{mrk-\
+Irns}$xlmw$jyr2$Xli$xvyxl${mpp$fi$liph$f}$xlswi${ls$ger$higshi$xli$qiwweki2$>-";
+
+
+int main()
+{
+    int len = strlen(msg);
+
+    int i;
+    for(i = 0; i < len; i++) {
+        int dec = (msg[i]) - 4;
+        printf("%c", dec);
+    }
+    return 0;
+}
+
+
+
+
+
+
+
+
