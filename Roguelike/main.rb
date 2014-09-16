32b03a28a28ebe07c1101ce3cb004899419ad42e
jakenotjacob <jake.campbell91@gmail.com>
2014-09-15 21:50:08 -0600

    Add unicode characters for entities.

--- Roguelike/main.rb
+++ Roguelike/main.rb
@@ -1,10 +1,31 @@
 #!/usr/bin/env ruby
 
+#Unicode symbols
+#http://jrgraphix.net/r/Unicode/2600-26FF
+  #print unicode chars
+  #def uni(from, to)
+  #  from.upto(to) do |i|
+  #    puts "%04x" % i + ": " + [i].pack("U*")
+  #  end
+  #end
+
 class Dungeon
   ROWS = 20
   COLS = 20
   ENTITIES = {
-    border: "\u2588" # Unicode char █
+    player: {
+      up: "\u25B2",   #▲
+      down: "\u25BC", #▼
+      left: "\u25C4", #◄
+      right: "\u25BA",#►
+    },
+    element: {
+      gold: "\u229B", # ⊛
+      plants: ["\u2660", "\u234B", "\u2227"], #♠ ⍋ ∧
+    },
+    border: "\u2588", #  █
+    ground: "\u2591", #  ░
+    stairs: "\u259F", #  ▟
   }
 
   attr_accessor :map
@@ -30,7 +51,7 @@ class Dungeon
     @map.each { |row|
       row.each do |cell|
         if cell.nil? 
-          print " "
+          print ENTITIES[:ground]
         else
           print cell
         end
@@ -43,3 +64,5 @@ end
 
 game = Dungeon.new
 game.draw_map
+
+
