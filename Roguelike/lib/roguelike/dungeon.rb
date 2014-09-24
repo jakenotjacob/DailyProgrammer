d1b6bfaef4e61083c9f5745f68ada811a6ae1ee0
jakenotjacob <jake.campbell91@gmail.com>
2014-09-24 15:15:08 -0600

    Add default-fill to floor. Add wall generation.

--- Roguelike/lib/roguelike/dungeon.rb
+++ Roguelike/lib/roguelike/dungeon.rb
@@ -3,21 +3,43 @@ module Roguelike
   class Dungeon
     attr_reader :grid
     TILE = {
-      #wall: "\u2588",   #  █
+      wall: "\u2588",   #  █
       ground: "\u2591", #  ░
       #stairs: "\u259F"  #  ▟
     }
 
+    HEIGHT = 20
+    WIDTH = 20
+
     def initialize
       puts "A dungeon entrance lurks in the darkness.."
-      @grid = Array.new(20) { Array.new(20) }
+      @grid = Array.new(HEIGHT) { Array.new(WIDTH, TILE[:ground]) }
+      post_init()
+    end
+
+    def post_init
+      add_walls
+    end
+
+    def add_walls
+      @grid[0].fill(TILE[:wall])
+      @grid[HEIGHT-1].fill(TILE[:wall])
+      @grid.each{ |row|
+        row.each { |tile|
+          if (tile == row.first)
+            @grid[@grid.index(row)][0] = TILE[:wall]
+          elsif (tile == row.last)
+            @grid[@grid.index(row)][WIDTH-1] = TILE[:wall]
+          end
+        }
+      }
     end
 
     def draw
       @grid.each { |row|
         row.each do |tile|
           if tile == nil
-            print TILE[:ground]
+            print " "
           else
             print tile
           end
@@ -32,28 +54,32 @@ module Roguelike
     end
 
     def clear_prev_tile(location)
-      @grid[ location[:y] ][ location[:x] ] = nil
+      @grid[ location[:y] ][ location[:x] ] = TILE[:ground]
     end
 
     def check_move(player, direction)
       case direction
       when "A"
-        if @grid[ player.location[:y]-1 ][ player.location[:x] ] == nil
+        next_move = @grid[ player.location[:y]-1 ][ player.location[:x] ]
+        if (next_move != nil)
           clear_prev_tile(player.location)
           player.move(direction)
         end
       when "B"
-        if @grid[ player.location[:y]+1 ][ player.location[:x] ] == nil
+        next_move = @grid[ player.location[:y]+1 ][ player.location[:x] ]
+        if (next_move != nil)
           clear_prev_tile(player.location)
           player.move(direction)
         end
       when "C"
-        if @grid[ player.location[:y] ][ player.location[:x]+1 ] == nil
+        next_move = @grid[ player.location[:y] ][ player.location[:x]+1 ]
+        if (next_move != nil)
           clear_prev_tile(player.location)
           player.move(direction)
         end
       when "D"
-        if @grid[ player.location[:y] ][ player.location[:x]-1 ] == nil
+        next_move = @grid[ player.location[:y] ][ player.location[:x]-1 ]
+        if (next_move != nil)
           clear_prev_tile(player.location)
           player.move(direction)
         end
